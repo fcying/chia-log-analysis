9dcd9acd2a9e58cb66cce07140850cdd6da77ef7
fcying <fcyingmk2@gmail.com>
2021-07-25 14:12:50 +0800

    use madmax

--- analysis.py
+++ analysis.py
@@ -29,12 +29,36 @@ TOTAL_TIME  = r"^Total time = (\d+\.\d+) seconds. CPU \((\d+\.\d+)%\)"
 COPY_TIME   = r"^Copy time = (\d+\.\d+) seconds.*\) (.*)"
 FILENAME    = r"^Renamed final file from \".+\" to \"(.+)\""
 
+MY_START        = r"start new plot: (.*)"
+MADMAX_TITLE    = r"^Multi-threaded pipelined Chia"
+MM_DIR          = r"Final Directory: (.*)"
+MM_TMP1         = r"Working Directory:\s+(.*)"
+MM_TMP2         = r"Working Directory 2:\s+(.*)"
+MM_NUM          = r"Number of Plots: (\d+)"
+MM_ID           = r"Process ID: (\d+)"
+MM_THREADS      = r"Number of Threads: (\d+)"
+MM_BUCKETS      = r"Number of Buckets P1:.*\((.*)\)"
+MM_START        = r"Plot Name: "
+MM_P1_TIME      = r"\[P1\] Table.*took (\d+)"
+MM_P1_END       = r"Phase 1 took (\d+)"
+MM_P2_TIME      = r"\[P2\] Table.*took (\d+)"
+MM_P2_END       = r"Phase 2 took (\d+)"
+MM_P3_TIME      = r"\[P3.*Table.*took (\d+)"
+MM_P3_END       = r"Phase 3 took (\d+)"
+MM_P4_END       = r"Phase 4 took (\d+)"
+MM_PARSE_END    = r"Total plot creation time was (\d+)"
+MM_COPY_START   = r"^Started copy to (.*)"
+MM_COPY_END     = r"^Copy to.*finished, took (\d+)"
+MM_FILENAME     = r"^Renamed final plot to (.*)"
+
 plot_list = []
 progress_sum = 0
+plot_type = "madmax"
 
 
 class plot_data:
     def __init__(self):
+        self.is_madmax = 0
         self.tmp_dir1 = ""
         self.tmp_dir2 = ""
         self.plot_id = ""
@@ -68,80 +92,152 @@ def open_log(file):
     parse_step = 0
     percent = 0
     with open(file, "r") as f:
-        for line in f:
-            if (ret := re.search(TMP_DIRS, line)) != None:
+        lines = f.readlines()
+        for line in lines:
+            if (ret := re.search(MY_START, line)) != None:
+                new_plot.start_time = datetime.strptime(ret.group(1), "%Y/%m/%d-%H:%M")
+            elif (ret := re.search(TMP_DIRS, line)) != None:
                 plot_list.append(new_plot)
                 new_plot.tmp_dir1 = ret.group(1)
                 new_plot.tmp_dir2 = ret.group(2)
-            elif (ret := re.search(PLOT_ID, line)) != None:
-                new_plot.plot_id = ret.group(1)
-            elif (ret := re.search(PLOT_SIZE, line)) != None:
-                new_plot.plot_size = ret.group(1)
-            elif (ret := re.search(BUFFER_SIZE, line)) != None:
-                new_plot.buffer_size = ret.group(1)
-            elif (ret := re.search(BUCKETS, line)) != None:
-                new_plot.buckets = int(ret.group(1))
-            elif (ret := re.search(THREADS, line)) != None:
-                new_plot.threads = ret.group(1)
-                new_plot.stripe_size = ret.group(2)
-            elif (ret := re.search(START_TIME, line)) != None:
-                new_plot.start_time = datetime.strptime(ret.group(1), "%a %b %d %H:%M:%S %Y")
-                parse_step = 1
-                new_plot.progress = 0
-                percent = PHASE1_WEIGHT / 7 / new_plot.buckets;
-            elif (ret := re.search(COPY_TIME, line)) != None:
-                new_plot.copy_time = float(ret.group(1))
-            elif (ret := re.search(PHASE_1, line)) != None:
-                new_plot.phase1_time = float(ret.group(1))
-                new_plot.phase1_cpu = ret.group(2)
-                parse_step = 2
-                new_plot.progress = PHASE1_WEIGHT
-                percent = PHASE2_WEIGHT / 7
-            elif (ret := re.search(PHASE_2, line)) != None:
-                new_plot.phase2_time = float(ret.group(1))
-                new_plot.phase2_cpu = ret.group(2)
-                parse_step = 3
-                new_plot.progress = PHASE1_WEIGHT + PHASE2_WEIGHT
-                percent = PHASE3_WEIGHT / 6
-            elif (ret := re.search(PHASE_3, line)) != None:
-                new_plot.phase3_time = float(ret.group(1))
-                new_plot.phase3_cpu = ret.group(2)
-                parse_step = 4
-                percent = PHASE4_WEIGHT / new_plot.buckets;
-                new_plot.progress = PHASE1_WEIGHT + PHASE2_WEIGHT + PHASE3_WEIGHT
-            elif (ret := re.search(PHASE_4, line)) != None:
-                new_plot.phase4_time = float(ret.group(1))
-                new_plot.phase1_cpu = ret.group(2)
-            elif (ret := re.search(TOTAL_TIME, line)) != None:
-                new_plot.total_time = float(ret.group(1))
-                new_plot.totalCpu = ret.group(2)
-                parse_step = 5
-                new_plot.elapsed_time = int(new_plot.total_time)
-                new_plot.phase1_weight = new_plot.phase1_time / new_plot.total_time * 100
-                new_plot.phase2_weight = new_plot.phase2_time / new_plot.total_time * 100
-                new_plot.phase3_weight = new_plot.phase3_time / new_plot.total_time * 100
-                new_plot.phase4_weight = new_plot.phase4_time / new_plot.total_time * 100
-            elif (ret := re.search(FILENAME, line)) != None:
-                parse_step = 0
-                new_plot.progress = 100
-                new_plot.filename = os.path.split(ret.group(1))[1]
-                new_plot = plot_data()
+                new_plot.is_madmax = 0
+            elif (ret := re.search(MADMAX_TITLE, line)) != None:
+                plot_list.append(new_plot)
+                new_plot.is_madmax = 1
+                new_plot.plot_size = "32"
+                new_plot.buffer_size = "madmax"
+                new_plot.elapsed_time = 0
 
-            if parse_step < 5:
-                new_plot.elapsed_time = int((datetime.now() - new_plot.start_time).total_seconds())
+            if new_plot.is_madmax:
+                if (ret := re.search(MM_ID, line)) != None:
+                    new_plot.plot_id = ret.group(1)
+                elif (ret := re.search(MM_TMP1, line)) != None:
+                    new_plot.tmp_dir1 = ret.group(1)
+                elif (ret := re.search(MM_TMP2, line)) != None:
+                    new_plot.tmp_dir2 = ret.group(1)
+                elif (ret := re.search(MM_THREADS, line)) != None:
+                    new_plot.threads = ret.group(1)
+                elif (ret := re.search(MM_BUCKETS, line)) != None:
+                    new_plot.buckets = int(ret.group(1))
+                elif (ret := re.search(MM_START, line)) != None:
+                    parse_step = 1
+                    new_plot.progress = 0
+                    percent = PHASE1_WEIGHT / 7;
+                elif (ret := re.search(MM_P1_TIME, line)) != None:
+                    new_plot.progress += percent
+                elif (ret := re.search(MM_P1_END, line)) != None:
+                    parse_step = 2
+                    new_plot.phase1_time = int(ret.group(1))
+                    new_plot.progress = PHASE1_WEIGHT
+                    percent = PHASE2_WEIGHT / 12
+                elif (ret := re.search(MM_P2_TIME, line)) != None:
+                    new_plot.progress += percent
+                elif (ret := re.search(MM_P2_END, line)) != None:
+                    parse_step = 3
+                    new_plot.phase2_time = int(ret.group(1))
+                    new_plot.progress = PHASE1_WEIGHT + PHASE2_WEIGHT
+                    percent = PHASE3_WEIGHT / 12
+                elif (ret := re.search(MM_P3_TIME, line)) != None:
+                    new_plot.progress += percent
+                    new_plot.phase3_time = int(ret.group(1))
+                elif (ret := re.search(MM_P3_END, line)) != None:
+                    parse_step = 4
+                    new_plot.phase3_time = int(ret.group(1))
+                    new_plot.progress = PHASE1_WEIGHT + PHASE2_WEIGHT + PHASE3_WEIGHT
+                    percent = PHASE4_WEIGHT / 4
+                elif (ret := re.search(MM_P4_END, line)) != None:
+                    parse_step = 5
+                    new_plot.phase4_time = int(ret.group(1))
+                elif (ret := re.search(MM_PARSE_END, line)) != None:
+                    parse_step = 5
+                    new_plot.elapsed_time = int(ret.group(1))
+                    new_plot.total_time = new_plot.elapsed_time
+                    new_plot.phase1_weight = new_plot.phase1_time / new_plot.total_time * 100
+                    new_plot.phase2_weight = new_plot.phase2_time / new_plot.total_time * 100
+                    new_plot.phase3_weight = new_plot.phase3_time / new_plot.total_time * 100
+                    new_plot.phase4_weight = new_plot.phase4_time / new_plot.total_time * 100
+                elif (ret := re.search(MM_COPY_START, line)) != None:
+                    new_plot.filename = os.path.split(ret.group(1))[1]
+                elif (ret := re.search(MM_COPY_END, line)) != None:
+                    parse_step = 0
+                    new_plot.progress = 100
+                    new_plot.copy_time = int(ret.group(1))
+                    new_plot = plot_data()
+                elif (ret := re.search(MM_FILENAME, line)) != None:
+                    parse_step = 0
+                    new_plot.progress = 100
+                    new_plot = plot_data()
+                if parse_step < 5:
+                    new_plot.elapsed_time = int((datetime.now() - new_plot.start_time).total_seconds())
+            else:
+                if (ret := re.search(PLOT_ID, line)) != None:
+                    new_plot.plot_id = ret.group(1)
+                elif (ret := re.search(PLOT_SIZE, line)) != None:
+                    new_plot.plot_size = ret.group(1)
+                elif (ret := re.search(BUFFER_SIZE, line)) != None:
+                    new_plot.buffer_size = ret.group(1)
+                elif (ret := re.search(BUCKETS, line)) != None:
+                    new_plot.buckets = int(ret.group(1))
+                elif (ret := re.search(THREADS, line)) != None:
+                    new_plot.threads = ret.group(1)
+                    new_plot.stripe_size = ret.group(2)
+                elif (ret := re.search(START_TIME, line)) != None:
+                    new_plot.start_time = datetime.strptime(ret.group(1), "%a %b %d %H:%M:%S %Y")
+                    parse_step = 1
+                    new_plot.progress = 0
+                    percent = PHASE1_WEIGHT / 7 / new_plot.buckets;
+                elif (ret := re.search(COPY_TIME, line)) != None:
+                    new_plot.copy_time = float(ret.group(1))
+                elif (ret := re.search(PHASE_1, line)) != None:
+                    new_plot.phase1_time = float(ret.group(1))
+                    new_plot.phase1_cpu = ret.group(2)
+                    parse_step = 2
+                    new_plot.progress = PHASE1_WEIGHT
+                    percent = PHASE2_WEIGHT / 7
+                elif (ret := re.search(PHASE_2, line)) != None:
+                    new_plot.phase2_time = float(ret.group(1))
+                    new_plot.phase2_cpu = ret.group(2)
+                    parse_step = 3
+                    new_plot.progress = PHASE1_WEIGHT + PHASE2_WEIGHT
+                    percent = PHASE3_WEIGHT / 6
+                elif (ret := re.search(PHASE_3, line)) != None:
+                    new_plot.phase3_time = float(ret.group(1))
+                    new_plot.phase3_cpu = ret.group(2)
+                    parse_step = 4
+                    percent = PHASE4_WEIGHT / new_plot.buckets;
+                    new_plot.progress = PHASE1_WEIGHT + PHASE2_WEIGHT + PHASE3_WEIGHT
+                elif (ret := re.search(PHASE_4, line)) != None:
+                    new_plot.phase4_time = float(ret.group(1))
+                    new_plot.phase1_cpu = ret.group(2)
+                elif (ret := re.search(TOTAL_TIME, line)) != None:
+                    new_plot.total_time = float(ret.group(1))
+                    new_plot.totalCpu = ret.group(2)
+                    parse_step = 5
+                    new_plot.elapsed_time = int(new_plot.total_time)
+                    new_plot.phase1_weight = new_plot.phase1_time / new_plot.total_time * 100
+                    new_plot.phase2_weight = new_plot.phase2_time / new_plot.total_time * 100
+                    new_plot.phase3_weight = new_plot.phase3_time / new_plot.total_time * 100
+                    new_plot.phase4_weight = new_plot.phase4_time / new_plot.total_time * 100
+                elif (ret := re.search(FILENAME, line)) != None:
+                    parse_step = 0
+                    new_plot.progress = 100
+                    new_plot.filename = os.path.split(ret.group(1))[1]
+                    new_plot = plot_data()
 
-            if parse_step == 1:
-                if (ret := re.search(r"^\sBucket", line)) != None:
-                    new_plot.progress += percent
-            elif parse_step == 2:
-                if (ret := re.search(r"^Backpropagating", line)) != None:
-                    new_plot.progress += percent
-            elif parse_step == 3:
-                if (ret := re.search(r"^Compressing tables", line)) != None:
-                    new_plot.progress += percent
-            elif parse_step == 4:
-                if (ret := re.search(r"^\sBucket", line)) != None:
-                    new_plot.progress += percent
+                if parse_step < 5:
+                    new_plot.elapsed_time = int((datetime.now() - new_plot.start_time).total_seconds())
+                    if parse_step == 1:
+                        if (ret := re.search(r"^\sBucket", line)) != None:
+                            new_plot.progress += percent
+                    elif parse_step == 2:
+                        if (ret := re.search(r"^Backpropagating", line)) != None:
+                            new_plot.progress += percent
+                    elif parse_step == 3:
+                        if (ret := re.search(r"^Compressing tables", line)) != None:
+                            new_plot.progress += percent
+                    elif parse_step == 4:
+                        if (ret := re.search(r"^\sBucket", line)) != None:
+                            new_plot.progress += percent
 
 def conversion_float_time(time):
     try:
@@ -164,7 +260,7 @@ def analysis_log():
 
     tb = PrettyTable()
 
-    title_list = ["k", "Buffer", "Thread", "TmpDir", "StartTime", "ElapsedTime", "PhaseTime", "Progress", "CopyTime"]
+    title_list = ["k", "Buckets", "Thread", "TmpDir", "StartTime", "ElapsedTime", "PhaseTime", "Progress", "CopyTime"]
     if options.filename:
         title_list.append("PlotFileName")
     if options.phaseweight:
@@ -198,7 +294,7 @@ def analysis_log():
             if plot.progress < int(options.progress):
                 progress_sum += 1
 
-        row = [plot.plot_size, plot.buffer_size, plot.threads, plot.tmp_dir1,
+        row = [plot.plot_size, plot.buckets, plot.threads, plot.tmp_dir1,
                plot.start_time.strftime(TIME_FORMAT),
                conversion_float_time(plot.elapsed_time),
                phase_time,
--- plot
+++ plot
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 #git clone --depth 1 https://github.com/Chia-Network/chia-blockchain.git
+#git clone https://github.com/madMAx43v3r/chia-plotter.git
 
 workdir=$(pwd)
 tmp=$workdir/tmp
 data=$workdir/data
 
+[ -z "$CLOGS" ] && CLOGS=$HOME/chialogs
+mkdir -p $CLOGS
+
 [ -z "$wait_progress" ] && wait_progress=5
 [ -z "$wait_count" ] && wait_count=2
 plot=1
-K=32
-thread=2
-buckets=64
-buffersize=3400
-once=0
+thread=""
+buckets=""
+number=0
 local_config=0
 while [[ $# -ge 1 ]]; do
     case $1 in
@@ -21,33 +23,30 @@ while [[ $# -ge 1 ]]; do
             echo "quit bin plot"
             while true
             do
-                pid=$(pgrep -af "(bin/plot.*-p|plots create)" | head -1 | awk '{print $1}')
+                pid=$(pgrep -af "(bin/plot.*-p|chia_plot)" | head -1 | awk '{print $1}')
                 if [ -n "$pid" ]; then
                     kill $pid
                 else
                     break
                 fi
             done
+            rm -f $CLOGS/*.log
             exit 0
             ;;
         -p)
             shift && plot="$1" && shift;;
-        -k)
-            shift && K="$1" && shift;;
-        -b)
-            shift && buffersize="$1" && shift;;
         -u)
-            shift && buckets="$1" && shift;;
+            shift && buckets="-u $1" && shift;;
         -r)
-            shift && thread="$1" && shift;;
+            shift && thread="-r $1" && shift;;
         -w)
             shift && wait_count="$1" && shift;;
         -t)
             shift && tmp="$1" && shift;;
         -d)
             shift && data="$1" && shift;;
-        -o)
-            once=1 && shift;;
+        -n)
+            shift && number="$1" && shift;;
         -l)
             local_config=1 && shift;;
         *)
@@ -56,20 +55,8 @@ while [[ $# -ge 1 ]]; do
     esac
 done
 
-if [ -z "$buffersize" ]; then
-    if [ "$K" -eq 33 ]; then
-        buffersize=7400
-    else
-        buffersize=6800
-    fi
-fi
-
-[ -n "$farmerpk" ] && farmerpk="-f $farmerpk"
-[ -n "$poolpk" ] && poolpk="-p $poolpk"
-[ -z "$CHOME" ] && CHOME=$HOME/chia-blockchain
-[ -z "$CLOGS" ] && CLOGS=$HOME/chialogs
-
-mkdir -p $CLOGS
+[ -z "$poolca" ] && echo "must set poolca(Pool Contract Address)" && exit 0
+[ -z "$farmerpk" ] && echo "must set farmerpk" && exit 0
 
 dir=$(basename $tmp)
 tmp=$tmp/$plot
@@ -78,18 +65,17 @@ if [ -z "$wait" ]; then
     wait=$[(plot-1)*20]
 fi
 
-echo "dir=$dir tmp:$tmp plot:$plot wait:${wait}m k=$K buffersize:$buffersize thread:$thread"
+echo "dir=$dir tmp:$tmp plot:$plot wait:${wait}m thread:$thread number:$number"
 #exit
 
-cd $CHOME
-. ./activate
 sleep ${wait}m
 
 logfile=$CLOGS/$dir$plot.log
-rm -f $logfile
+current_loop=1
 while true
 do
     rm -rf $tmp
+    mkdir -p $tmp
     while true
     do
         analysis -p $wait_progress
@@ -100,20 +86,24 @@ do
             break
         fi
     done
+    rm -f $logfile
 
-    chia plots create -k $K -b $buffersize -r $thread -u $buckets $farmerpk $poolpk -t $tmp -d $data > $logfile
+    echo "start new plot: $(date +%Y/%m/%d-%H:%M)" | tee -a $logfile
+    chia_plot -n 1 $buckets $thread -t "$tmp/" -d "$data/" -f $farmerpk -c $poolca | tee -a $logfile
 
-    if [ $(grep -c "Renamed final file from" $logfile) -ne 0 ]; then
+    if [ $(grep -c "Renamed final" $logfile) -ne 0 ] || [ $(grep -Pc "Copy to.*finished" $logfile) -ne 0 ]; then
+        echo "move log file"
         mkdir -p $CLOGS/end && mv $logfile $_/$dir$plot-$(date +%y%m%d%H%M).log
     fi
     if [ "$local_config" -eq 1 ]; then
         source ~/bin/plot.local
     fi
-    if [ "$once" -eq 1 ]; then
+    if [ "$current_loop" -eq "$number" ]; then
         echo "plot end"
         exit 0
     fi
-
-    echo "start next plot"
+    echo "start next plot, sleep 2, loop_count: $current_loop"
+    sleep 2
+    current_loop=$[current_loop+1]
 done
 
